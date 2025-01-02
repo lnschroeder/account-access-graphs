29cf1ccbc6df339b6334d120ab50d758a3160152
lnschroeder <33390735+lnschroeder@users.noreply.github.com>
2025-01-02 21:32:11 +0100

    Enable second page of addaccess again

--- src/client/fsharp/pages/AddAccessPage.fs
+++ src/client/fsharp/pages/AddAccessPage.fs
@@ -23,47 +23,36 @@ let selectSubjectByName name (model: Model) =
             subjectInput =
                 { value = name
                   hint = Hint.Error "Vertex does not exist" } }
-let selectFactorById (idAsString: string) (model: Model) =
-    // let hint =
-    //     if name = "" then
-    //         Input.VertexForNewAccessInput.hint
-    //     elif not (AAG.isVertexWithNameInGraph name model.graph) then
-    //         Hint.Error "Vertex does not exist"
-    //     else
-    //         Hint.Info
-    let name =
-        if idAsString = "undefined" then
-            ""
-        else
-            let guid = Guid.Parse idAsString
-            let vertex = AAG.findVertexById guid model.graph
 
-            match vertex with
-            | Some vertex -> vertex.name
-            | None -> ""
-
+let toggleFactor (vertex: AAG.Vertex) (model: Model) =
     let factors =
-        if List.contains name model.factorsInput then
-            List.filter ((<>) name) model.factorsInput
+        if List.contains vertex.name model.factorsInput then
+            List.filter ((<>) vertex.name) model.factorsInput
         else
-            name :: model.factorsInput
+            vertex.name :: model.factorsInput
 
     { model with factorsInput = factors }
 
 let cancel model =
-    { deselectSubject model with
+    { model with
+        subjectInput = Input.SubjectInput
+        factorsInput = []
         page = Endpoint.MainMenu
         step = None }
 
 let continueSubject model = { model with step = Some "factors" }
 
 let handleClickedVertex (vertex: AAG.Vertex option) (model: Model) =
-    if model.step = None then
+    match model.step with
+    | None ->
         match vertex with
         | Some vertex -> selectSubject vertex model
         | None -> deselectSubject model
-    else
-        model
+    | Some "factors" ->
+        match vertex with
+        | Some vertex -> toggleFactor vertex model
+        | _ -> model
+    | _ -> model
 
 let view jsRuntime (model: Model) dispatch =
     Utility.disableButton "ContinueButton" (isInvalidInput model.subjectInput) jsRuntime
@@ -80,9 +69,6 @@ let view jsRuntime (model: Model) dispatch =
             .AddAccessSubject()
             .CancelButton(fun _ -> dispatch Msg.ClickedCancelAddAccessButton)
             .ContinueButton(fun _ -> dispatch Msg.ClickedContinueSubject)
-            .VertexNameInput(
-                model.subjectInput.value,
-                (fun v -> dispatch (Msg.TypedSubjectName v))
-            )
+            .VertexNameInput(model.subjectInput.value, (fun v -> dispatch (Msg.TypedSubjectName v)))
             .VertexNameHint(model.subjectInput.hint.value)
             .Elt()
