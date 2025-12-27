f2300eca436b0d490cee92a927022bafaf83c3de
lnschroeder <33390735+lnschroeder@users.noreply.github.com>
2025-12-27 17:13:44 +0100

    Use on-surface color instead of surface-variant
    The surface-variant color is a bit more gray; on-surface is white/black and this also conforms with Material design

--- frontend/src/stores/visNetworkOptionsStore.ts
+++ frontend/src/stores/visNetworkOptionsStore.ts
@@ -1,47 +1,48 @@
 import { defineStore } from 'pinia'
-import type { Options } from 'vis-network'
+import type { Options, Font } from 'vis-network'
 import { ref, computed } from 'vue'
 import { useTheme } from 'vuetify'
 
 export const useVisNetworkOptionsStore = defineStore('visNetworkOptions', () => {
   const theme = useTheme()
   const physicsEnabled = ref(true)
+
   const fontFace = 'Roboto, Arial'
 
   const options = computed<Options>(() => {
     const colors = theme.current.value.colors
+    const surfaceColor = colors['surface']
+    const onSurfaceColor = colors['on-surface']
+
+    const font: Font = {
+      face: fontFace,
+      color: onSurfaceColor,
+      background: surfaceColor,
+      strokeWidth: 0,
+    }
+
     return {
-      layout: {
-        randomSeed: 0,
-      },
+      layout: { randomSeed: 0 },
+
+      physics: { enabled: physicsEnabled.value },
+
       nodes: {
         shape: 'box',
+        shapeProperties: { borderRadius: 4 },
         chosen: false,
         color: {
-          background: colors['surface'],
-          border: colors['surface-variant'],
+          background: surfaceColor,
+          border: onSurfaceColor,
         },
-        font: {
-          color: colors['surface-variant'],
-          face: fontFace,
-        },
+        font: font,
       },
+
       edges: {
-        color: colors['surface-variant'],
-        arrows: {
-          to: {
-            enabled: true,
-          },
-        },
+        color: onSurfaceColor,
+        arrows: { to: { enabled: true } },
         chosen: false,
-        font: {
-          face: fontFace,
-          color: colors['surface-variant'],
-          background: colors['surface'],
-          strokeWidth: 0,
-        },
+        font: font,
       },
-      physics: { enabled: physicsEnabled.value },
     }
   })
 
